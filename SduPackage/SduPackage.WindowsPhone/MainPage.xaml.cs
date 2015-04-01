4ba4341b0342652376cd5071146e7cafe204c0d0
Rubbly <smy641806232@live.com>
2015-04-02 02:17:03 +0800

    NewsV

--- SduPackage/SduPackage.WindowsPhone/MainPage.xaml.cs
+++ SduPackage/SduPackage.WindowsPhone/MainPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using SduPackage.Funcitons;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System.Threading;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,15 +24,14 @@ namespace SduPackage
 
     public sealed partial class MainPage : Page
     {
-        Frame rootFrame;
+        private Windows.Storage.StorageFolder _localFolder;
+
 
         public MainPage()
         {
             this.InitializeComponent();
-
+            this._localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
             this.NavigationCacheMode = NavigationCacheMode.Required;
-            rootFrame = Window.Current.Content as Frame;
-            this.rootFrame.Navigate(typeof(Views.Index));
         }
 
         /// <summary>
@@ -40,14 +41,42 @@ namespace SduPackage
         /// 此参数通常用于配置页。</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            this.rootFrame.Navigate(typeof(Views.Index));
+            this.Frame.Navigate(typeof(Views.Index));
+            downToFile();
+            ThreadPoolTimer.CreateTimer(async t =>
+            {
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    isDowning.IsActive = false;
+                    Frame.Navigate(typeof(Views.Index));
+                });
+
+            }, TimeSpan.FromSeconds(2));
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void downToFile()
         {
-            Frame.Navigate(typeof(Views.Index));
+            var http = new DoPost();
+            //学线通知
+            http.StartPost("http://www.online.sdu.edu.cn/News2s/servlet/NewestListServlet", "", result =>
+            {
+                result = result.Substring(2, result.Length - 2);
+                SaveFile("TheNewsFromSduOnline.txt", result);
+            });
+            //其他通知
+            http.StartPost("http://www.online.sdu.edu.cn/News2s/servlet/PassageListServlet", "id=0&page=1", result =>
+            {
+                result = result.Substring(2, result.Length - 2);
+                SaveFile("TheNewsFromOthers.txt", result);
+            });
+            
         }
 
+        private async void SaveFile(string FileName, string result)
+        {
+            Windows.Storage.StorageFile tempFile = await _localFolder.CreateFileAsync(FileName, Windows.Storage.CreationCollisionOption.ReplaceExisting);
+            await Windows.Storage.FileIO.WriteTextAsync(tempFile, result);
+        }
 
     }
 }
