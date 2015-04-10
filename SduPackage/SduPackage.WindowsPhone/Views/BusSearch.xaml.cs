1d1e452eba7389161f49b5cdbca54bdcf559fcea
Rubbly <641806232@qq.com>
2015-04-11 00:04:15 +0800

    0.0.0.1

---
+++ SduPackage/SduPackage.WindowsPhone/Views/BusSearch.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkID=390556 上有介绍
+
+namespace SduPackage.Views
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class BusSearch : Page
+    {
+        int count = 0;
+
+        public BusSearch()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            Windows.Phone.UI.Input.HardwareButtons.BackPressed -= HardwareButtons_BackPressed;
+
+        }
+        
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Windows.Phone.UI.Input.HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+
+        }
+        #region 页面点击事件
+        private void PlaceButtonTap(object sender, TappedRoutedEventArgs e)
+        {
+            count++;
+            var choosePlaceBorder = sender as Border;
+            int choosePlaceNum = Int32.Parse((string)choosePlaceBorder.Tag);
+            if ((count % 2) == 1)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+        #endregion
+
+        #region 事件
+        public void setStartPlace(int i)
+        {
+
+        }
+
+        public void setEndPlace(int i)
+        {
+
+        }
+
+        void HardwareButtons_BackPressed(object sender, Windows.Phone.UI.Input.BackPressedEventArgs e)
+        {
+            e.Handled = true;
+            if (this.Frame.CanGoBack)
+            {
+                this.Frame.GoBack();
+            }
+        }
+        #endregion
+        
+        
+    }
+}
