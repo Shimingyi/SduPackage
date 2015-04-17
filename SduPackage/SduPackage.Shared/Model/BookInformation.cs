97634318d222ba200c2655de8af4ac267e6cdc61
Rubbly <641806232@qq.com>
2015-04-17 23:48:34 +0800

    Add Book Model and User Model

---
+++ SduPackage/SduPackage.Shared/Model/BookInformation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SduPackage.Model
+{
+    public class BookInformation
+    {
+        //Book user information
+        public string b_id { get; set; }
+        public string b_title { get; set; }
+        public string b_data { get; set; }
+
+
+        //Book search information
+        public string b_total { get; set; }
+        public string b_title { get; set; }
+        public string b_detailurl { get; set; }
+        public string b_number { get; set; }
+        public string b_canborrow { get; set; }
+
+        //Book browse state
+        public string b_location { get; set; }
+        public string b_state { get; set; }
+
+        
+    }
+}
---
+++ SduPackage/SduPackage.Shared/Model/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SduPackage.Model
+{
+    public class User
+    {
+        #region 属性
+        /// <summary>
+        /// The name of User
+        /// </summary>
+        public string myName { get; set; }
+
+        /// <summary>
+        /// The stuNum of User
+        /// </summary>
+        public string myStudentID { get; set; }
+
+        /// <summary>
+        /// The academy of User like '软件学院'
+        /// </summary>
+        public string myAcademy { get; set; }
+
+        /// <summary>
+        /// The academy of User like '软件工程'
+        /// </summary>
+        public string mySpecialty { get; set; }
+
+        /// <summary>
+        /// The grade of first academy
+        /// </summary>
+        public string firstAveGrade { get; set; }
+
+        /// <summary>
+        /// The grade of second academy
+        /// </summary>
+        public string secondAveGrade { get; set; }
+
+        /// <summary>
+        /// The grade of third academy
+        /// </summary>
+        public string thirdAveGrade { get; set; }
+
+        /// <summary>
+        /// The grade of four academy
+        /// </summary>
+        public string forthAveGrade { get; set; }
+        #endregion
+    }
+}
