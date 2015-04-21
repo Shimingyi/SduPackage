f708a7eceffc1b7dbab5233de96807b9fcd89f60
Rubbly <iRubbly@outlook.com>
2015-04-21 23:44:23 +0800

    update book ViewModel

--- SduPackage/SduPackage.Shared/ViewModel/BookViewModel.cs
+++ SduPackage/SduPackage.Shared/ViewModel/BookViewModel.cs
@@ -13,32 +13,70 @@ namespace SduPackage.ViewModel{
 	public class BookViewModel: INotifyPropertyChanged
     {
     	#region 字段
-    	Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
-        Windows.Storage.StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+    	Windows.Storage.ApplicationDataContainer _localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+        Windows.Storage.StorageFolder _localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
 
     	#endregion
 
     	#region 属性
-    	#endregion
-    	public ObservableCollection<BusInformation> BookGroup
+        public ObservableCollection<BusInformation> BookGroup
         {
             get;
             private set;
         }
+    	#endregion
+    	
     	#region 事件
     	public event PropertyChangedEventHandler PropertyChanged;
     	#endregion
 
     	#region 构造方法
+        public BookViewModel(int index)
+        {
+            RaisePropertyChanged("BookGroup");
+        }
     	#endregion
-    	public BookViewModel(){
-    		RaisePropertyChanged("BookGroup");
-    	}
+
     	#region 方法
-    	
+        public void LoadBookGroup(int index)
+        {
+            downToFile(index);
+        }
+
+        public void ContinueBook()
+        {
+
+        }
+
+        public void ClearGroup()
+        {
+
+        }
     	#endregion
 
     	#region 私有方法
+        private void downToFile(int index){
+            switch(index){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        }
+
+        private void addToGroup(string result)
+        {
+
+        }
+
+        private async void SaveFile(string FileName, string result)
+        {
+            Windows.Storage.StorageFile tempFile = await _localFolder.CreateFileAsync(FileName, Windows.Storage.CreationCollisionOption.ReplaceExisting);
+            await Windows.Storage.FileIO.WriteTextAsync(tempFile, result);
+        }
+
     	private void RaisePropertyChanged(String propertyName)
         {
             if (this.PropertyChanged != null)
